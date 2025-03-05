329d3dcde8e0e87e887afdb2dafbaba4bb2b5598
Sumon Mitra <sumonmitra.21@gmail.com>
2025-03-05 17:21:49 +0530

    add new things

--- BE/index.ts
+++ BE/index.ts
@@ -4,16 +4,52 @@ import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 import cors from "cors";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 const prisma = new PrismaClient();
 const app = express();
 dotenv.config();
 app.use(express.json());
+app.use(cookieParser());
 app.use(
   cors({
     credentials: true,
   })
 );
+
+interface extendRequest extends Request {
+  userId?: string;
+}
+
+const authMiddleware = async (
+  req: extendRequest,
+  res: Response,
+  next: () => void
+): Promise<any> => {
+  try {
+    // const token = req.headers.authorization as string;
+    const token = req.cookies.token;
+    if (!token) {
+      return res.status(401).json("Unauthorized!");
+    }
+    const { id } = jwt.verify(token, process.env.JWT_SECRET as string) as any;
+    if (!id) {
+      return res.status(401).json("token doesn't match!");
+    }
+    const user = await prisma.user.findUnique({
+      where: {
+        id: id,
+      },
+    });
+    if (!user) {
+      return res.status(401).json("Can't find user!");
+    }
+    req.userId = user.id;
+    next();
+  } catch (error) {
+    return res.status(500).json("Internal middleware error!");
+  }
+};
 
 async function main() {
   app.get("/", (req, res): any => {
@@ -82,11 +118,105 @@ async function main() {
       return res.status(500).json("Internal server error!");
     }
   });
+
+  app.post(
+    "/create-todo",
+    authMiddleware,
+    async (req: extendRequest, res: Response): Promise<any> => {
+      const { title, description } = req.body;
+      if (!title) {
+        return res.status(400).json("Title is required!");
+      }
+      try {
+        const newPost = await prisma.todo.create({
+          data: {
+            title: title as string,
+            description: description as string,
+            userId: req.userId as string,
+          },
+        });
+        return res
+          .status(201)
+          .json({ message: "Post created..", post: newPost });
+      } catch (error) {
+        return res.status(500).json("Internal server error!");
+      }
+    }
+  );
+
+  app.get(
+    "/get-todos",
+    authMiddleware,
+    async (req: extendRequest, res: Response): Promise<any> => {
+      try {
+        const id = req.userId;
+        const todos = await prisma.todo.findMany({
+          where: {
+            userId: id,
+          },
+        });
+
+        if (!todos) {
+          return res.status(400).json("No todos found!");
+        }
+
+        return res.status(200).json(todos);
+      } catch (error) {
+        return res.status(500).json("Internal server error to get!!");
+      }
+    }
+  );
+
+  app.put(
+    "/edit-todo/:t_id",
+    authMiddleware,
+    async (req: extendRequest, res: Response): Promise<any> => {
+      try {
+        const { t_id } = req.params;
+        if (!t_id) {
+          return res.status(400).json("Todo id is required!");
+        }
+        const { title, description, isDone } = req.body;
+        if (!title && !description && !isDone) {
+          return res.status(400).json("At least one update is required!");
+        }
+
+        const todo = await prisma.todo.findUnique({
+          where: { id: t_id },
+        });
+        if (!todo) {
+          return res.status(400).json("No todo found!");
+        }
+
+        const updatedTodo = await prisma.todo.update({
+          where: { id: t_id },
+          data: {
+            title,
+            description,
+            isDone
+          },
+        });
+
+        return res.status(200).json(updatedTodo);
+      } catch (error) {
+        return res.status(500).json("Internal server error to edit!!");
+      }
+    }
+  );
 }
 
-main()
-  .catch((e) => console.error(e))
-  .finally(async () => await prisma.$disconnect());
+main().catch((e) => console.error(e));
+
+process.on("exit", async () => {
+  await prisma.$disconnect();
+  console.log("Prisma disconnected!");
+});
+
+process.on("SIGINT", async () => {
+  await prisma.$disconnect();
+  console.log("Prisma disconnected!");
+  process.exit(0);
+});
 
 app.listen(5000, () => {
   console.log(`Port listen on 5000`);
